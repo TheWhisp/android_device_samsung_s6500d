6fad4a8a60692c0567a45964c33a1eb67f8fca7d
Davis Malnieks <malnieks.davis@gmail.com>
2014-08-02 01:05:58 +0300

    jenad: update bluetooth config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,12 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "GT-S6500D"
+#define BTM_DEF_LOCAL_NAME "GT-S6500D"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BLUETOOTH_QCOM_SW TRUE
+
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
 
 #endif
